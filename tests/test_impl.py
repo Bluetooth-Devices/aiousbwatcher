6adf8d700c8823ade6d52d01588866c433537873
J. Nick Koston <nick@koston.org>
2025-01-15 11:10:51 -1000

    chore: add coverage for failing callbacks

--- tests/test_impl.py
+++ tests/test_impl.py
@@ -41,3 +41,38 @@ async def test_aiousbwatcher_callbacks(tmp_path: Path) -> None:
         stop()
         await asyncio.sleep(0.1)
         assert not called
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    platform != "linux", reason="Inotify not available on this platform"
+)
+async def test_aiousbwatcher_broken_callbacks(
+    tmp_path: Path, caplog: pytest.LogCaptureFixture
+) -> None:
+    called: bool = False
+
+    def callback() -> None:
+        nonlocal called
+        called = True
+
+    def broken_callback() -> None:
+        raise Exception("Broken")
+
+    with patch("aiousbwatcher.impl._PATH", str(tmp_path)):
+        watcher = AIOUSBWatcher()
+        unregister = watcher.async_register_callback(broken_callback)
+        unregister2 = watcher.async_register_callback(callback)
+        stop = watcher.async_start()
+        await asyncio.sleep(0.1)
+        assert not called
+        (tmp_path / "test").touch()
+        await asyncio.sleep(0.1)
+        assert called
+        called = False  # type: ignore[unreachable]
+        assert "Broken" in caplog.text
+        unregister()
+        unregister2()
+        stop()
+        await asyncio.sleep(0.1)
+        assert not called
