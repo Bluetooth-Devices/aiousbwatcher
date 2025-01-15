550307f7063a99f6956682166ecf716aa1c4d3db
J. Nick Koston <nick@koston.org>
2025-01-15 11:14:18 -1000

    chore: add coverage for failing callbacks (#2)

--- tests/test_impl.py
+++ tests/test_impl.py
@@ -7,6 +7,8 @@ import pytest
 
 from aiousbwatcher import AIOUSBWatcher, InotifyNotAvailableError
 
+_INOTIFY_WAIT_TIME = 0.2
+
 
 @pytest.mark.asyncio
 @pytest.mark.skipif(platform == "linux", reason="Inotify is available on this platform")
@@ -31,13 +33,48 @@ async def test_aiousbwatcher_callbacks(tmp_path: Path) -> None:
         watcher = AIOUSBWatcher()
         unregister = watcher.async_register_callback(callback)
         stop = watcher.async_start()
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(_INOTIFY_WAIT_TIME)
         assert not called
         (tmp_path / "test").touch()
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(_INOTIFY_WAIT_TIME)
         assert called
         called = False  # type: ignore[unreachable]
         unregister()
         stop()
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(_INOTIFY_WAIT_TIME)
+        assert not called
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    platform != "linux", reason="Inotify not available on this platform"
+)
+async def test_aiousbwatcher_broken_callbacks(
+    tmp_path: Path, caplog: pytest.LogCaptureFixture
+) -> None:
+    called: bool = False
+
+    def callback() -> None:
+        nonlocal called
+        called = True
+
+    def broken_callback() -> None:
+        raise Exception("Broken")
+
+    with patch("aiousbwatcher.impl._PATH", str(tmp_path)):
+        watcher = AIOUSBWatcher()
+        unregister = watcher.async_register_callback(broken_callback)
+        unregister2 = watcher.async_register_callback(callback)
+        stop = watcher.async_start()
+        await asyncio.sleep(_INOTIFY_WAIT_TIME)
+        assert not called
+        (tmp_path / "test").touch()
+        await asyncio.sleep(_INOTIFY_WAIT_TIME)
+        assert called
+        called = False  # type: ignore[unreachable]
+        assert "Broken" in caplog.text
+        unregister()
+        unregister2()
+        stop()
+        await asyncio.sleep(_INOTIFY_WAIT_TIME)
         assert not called
