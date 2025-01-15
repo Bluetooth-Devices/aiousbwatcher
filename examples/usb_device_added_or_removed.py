27a9a142589444f167ab5d576975fa3409c40776
J. Nick Koston <nick@koston.org>
2025-01-15 11:34:03 -1000

    feat: improve example to show how to stop the watcher (#6)

--- examples/usb_device_added_or_removed.py
+++ examples/usb_device_added_or_removed.py
@@ -7,14 +7,17 @@ async def main() -> None:
     """Main entry point of the program."""
     try:
         watcher = AIOUSBWatcher()
-        watcher.async_start()
+        cancel = watcher.async_start()
     except InotifyNotAvailableError as ex:
         print(ex)
         return
 
     watcher.async_register_callback(lambda: print("USB device added/removed"))
     event = asyncio.Event()
-    await event.wait()
+    try:
+        await event.wait()
+    finally:
+        cancel()
 
 
 if __name__ == "__main__":
